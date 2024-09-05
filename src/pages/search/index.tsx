b8085afa87948af2b6a007128834c220377747e1
miss-gif <ddc.akari@gmail.com>
2024-09-05 23:00:37 +0900

    Add : UI 작업 완료

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,13 +1,17 @@
 import SearchableLayout from "@/components/searchable-layout";
 import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
 
 const Page = () => {
-  const router = useRouter();
-
-  const { q } = router.query;
-
-  return <div>Page {q}</div>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 };
 
 export default Page;
